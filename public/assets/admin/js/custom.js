7033d7d3d442de9dfdae67bb617944a91e34c0cf
arif-setyo-wibowo <21081010057@student.upnjatim.ac.id>
2024-11-05 21:35:25 +0700

    templating first

---
+++ public/assets/admin/js/custom.js
@@ -0,0 +1,48 @@
+// Dismiss Alert
+setTimeout(function() {
+    document.getElementById("autoDismissAlert").style.display = "none";
+}, 3000);
+
+// Edit Kategori
+function editKategori(id, kategori, harga) {
+    $('[href="#tab-tambah-edit"]').tab("show");
+    $("#idkategori").val(id);
+    $("#kategori").val(kategori);
+    $("#harga").val(harga);
+    $("#proses").val("Update");
+}
+
+// Hapus Value Kategori
+$("#custom-tab-tambah-edit").on("click", function() {
+    $("#idkategori").val("");
+    $("#kategori").val("");
+    $("#harga").val("");
+    $("#proses").val("Tambah");
+});
+
+// Edit Pengguna
+function editPengguna(id, nama, username, password, role) {
+    $('[href="#tab-tambah-edit"]').tab("show");
+    $("#id").val(id);
+    $("#nama").val(nama);
+    $("#username").val(username);
+    $("#password_lama").val(password);
+    $("#role").val(role);
+    $("#notifPassword").text("*Kosongkan Jika Tidak Ingin Merubah Password");
+    $("#password").removeAttr("required");
+    $("#proses").val("Update");
+}
+
+// Tambah Value Pengguna
+$("#tambah-edit-tab").on("click", function() {
+    $("#idpengguna").val("");
+    $("#nama").val("");
+    $("#username").val("");
+    $("#email").val("");
+    $("#level").val("User");
+    $("#notifPassword").empty();
+    $("#password").prop("required", true);
+    $("#proses").val("Tambah");
+});
+
+// Edit Mobil
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,14 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Admin\AdminController;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::prefix('admin')->group(function () {
+    Route::controller(AdminController::class)->group(function () {
+        Route::get('/', 'index')->name('admin.index');
+    });
 });
